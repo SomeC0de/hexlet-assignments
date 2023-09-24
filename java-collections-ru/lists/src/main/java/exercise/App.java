1a4d9f6e14037de9581e18bc1e62c5bd95972d3e
Oleg Isanbaev <ingen.ole@yandex.ru>
2023-09-25 02:21:37 +0300

    Solving exercise

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,44 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+// BEGIN
+public class App {
+    public static boolean scrabble (String input, String word) {
+        boolean isScrabbled;
+
+        if (input.length() < word.length()) {
+            isScrabbled = false;
+        } else if ((input.isEmpty()) || (word.isEmpty())) {
+            isScrabbled = false;
+        } else {
+            // Array to ArrayList Conversion
+            String[] inputLetters = input.split("");
+            List<String> inputList = new ArrayList<>();
+            for (String letter : inputLetters)
+                inputList.add(letter);
+
+            // Array to ArrayList Conversion
+            String[] wordLetters = word.split("");
+            List<String> wordList = new ArrayList<>();
+            for (String letter : wordLetters)
+                wordList.add(letter);
+
+            boolean isFound = true;
+
+            for (String letter : wordList)
+                if (inputList.contains(letter)) {
+                    inputList.remove(letter);
+                } else {
+                    isFound = false;
+                    break;
+                }
+
+            isScrabbled = isFound;
+        }
+
+        return isScrabbled;
+    }
+}
+//END
