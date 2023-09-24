b1592e51f44063cd3d6ce54981679324b32d16e8
@hexlet/cli <support@hexlet.io>
2023-09-25 02:29:03 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,11 +1,10 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 // BEGIN
 public class App {
-    public static boolean scrabble (String input, String word) {
+    public static boolean scrabble(String input, String word) {
         boolean isScrabbled;
 
         if (input.length() < word.length()) {
@@ -16,24 +15,27 @@ public class App {
             // Array to ArrayList Conversion
             String[] inputLetters = input.split("");
             List<String> inputList = new ArrayList<>();
-            for (String letter : inputLetters)
+            for (String letter : inputLetters) {
                 inputList.add(letter);
+            }
 
             // Array to ArrayList Conversion
             String[] wordLetters = word.split("");
             List<String> wordList = new ArrayList<>();
-            for (String letter : wordLetters)
+            for (String letter : wordLetters) {
                 wordList.add(letter);
+            }
 
             boolean isFound = true;
 
-            for (String letter : wordList)
+            for (String letter : wordList) {
                 if (inputList.contains(letter)) {
                     inputList.remove(letter);
                 } else {
                     isFound = false;
                     break;
                 }
+            }
 
             isScrabbled = isFound;
         }
